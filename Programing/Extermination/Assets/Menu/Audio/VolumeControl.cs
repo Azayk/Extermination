7c766b1214520becce415e206ca00d0a3d7b4dae
Ka3u-S <165663358+Ka3u-S@users.noreply.github.com>
2024-04-07 20:08:52 +0800

    Menu 2

---
+++ Programing/Extermination/Assets/Menu/Audio/VolumeControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    public string volumeParameter = "MasterVolume";
+    public AudioMixer mixer;
+    public Slider SliderAudio;
+
+    private float _volumeValue;
+    private const float _asd = 20f;
+
+    private void Awake()
+    {
+        SliderAudio.onValueChanged.AddListener(HandleSliderValueChanged);
+    }
+
+    private void HandleSliderValueChanged(float value)
+    {
+        _volumeValue = Mathf.Log10(value) * _asd;
+        mixer.SetFloat(volumeParameter, _volumeValue);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _volumeValue = PlayerPrefs.GetFloat(volumeParameter, Mathf.Log10(SliderAudio.value) * _asd);
+        SliderAudio.value = Mathf.Pow(10f, _volumeValue / _asd);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(volumeParameter, _volumeValue);
+    }
+}
